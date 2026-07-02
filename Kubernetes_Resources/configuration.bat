f20b19ead2359750c205f8b5de33823d98d7a86c
Francesco Monzillo <fracmonz@gmail.com>
2026-07-02 16:04:21 +0200

    Fixed configuration.bat

--- Kubernetes_Resources/configuration.bat
+++ Kubernetes_Resources/configuration.bat
@@ -7,7 +7,7 @@ kubectl create secret generic db-secret --from-env-file=../mongodb/.env -n resum
 kubectl create -f ./Deployments
 kubectl create -f ./Services
 kubectl create -f "./Stateful Sets"
-kubectl exec -it mongodb-0 -- bash
-mongosh
-rs.initiate({_id: "rs0",members: [{ _id: 0, host: "mongodb-0.mongodb-service.resume-master.svc.cluster.local:27017" },{ _id: 1, host: "mongodb-1.mongodb-service.resume-master.svc.cluster.local:27017" },{ _id: 2, host: "mongodb-2.mongodb-service.resume-master.svc.cluster.local:27017" }]})
-exit
+:::kubectl exec -it mongodb-0 -- bash
+::mongosh
+::rs.initiate({_id: "rs0",members: [{ _id: 0, host: "mongodb-0.mongodb-service.resume-master.svc.cluster.local:27017" },{ _id: 1, host: "mongodb-1.mongodb-service.resume-master.svc.cluster.local:27017" },{ _id: 2, host: "mongodb-2.mongodb-service.resume-master.svc.cluster.local:27017" }]})
+::exit
